306dea4bb4eb9fa21081211d8323b8e24559f81a
George Nizor <gnizorid@gmail.com>
2025-04-01 23:30:04 +1100

    updated script to window_mode_maximised for intended functionality

--- Main.gd
+++ Main.gd
@@ -1,17 +1,7 @@
 extends Node
 
 func _ready():
+    DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
     var current_screen := DisplayServer.window_get_current_screen()
-    var screen_size := DisplayServer.screen_get_size(current_screen)
-    var screen_position := DisplayServer.screen_get_position(current_screen)
-
-    # Keep the window border
-    DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
-    
-    # Set size and position to match screen
-    DisplayServer.window_set_size(screen_size)
-    DisplayServer.window_set_position(screen_position)
-
-    print("Current screen:", current_screen)
-    print("Screen size:", screen_size)
-    print("Screen position:", screen_position)
+    print("Maximized on screen:", current_screen)
